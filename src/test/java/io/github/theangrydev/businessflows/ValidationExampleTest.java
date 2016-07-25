3e58be4d08cc3b0c40a1a7516ef69e2a3b82e4da
Liam Williams <liam.williams@zoho.com>
2016-07-25 18:55:14 +0100

    Refactoring

--- src/test/java/io/github/theangrydev/businessflows/ValidationExampleTest.java
+++ src/test/java/io/github/theangrydev/businessflows/ValidationExampleTest.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2016 Liam Williams <liam.williams@zoho.com>.
+ *
+ * This file is part of business-flows.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.theangrydev.businessflows;
 
 import org.junit.Test;
@@ -49,10 +66,11 @@ public class ValidationExampleTest {
 
     @Test
     public void validateRegistrationForm() {
-        validate(registrationForm())
+        String result = validate(registrationForm())
                 .peek(this::registerUser)
                 .ifTechnicalFailure().peek(this::logFailure)
                 .join(this::renderValidationErrors, this::renderJoinedPage, this::renderFailure);
+        System.out.println("result = " + result);
     }
 
     private ValidationPath<ValidationError, RegistrationForm> validate(RegistrationForm registrationForm) {
@@ -92,14 +110,14 @@ public class ValidationExampleTest {
     }
 
     private Validator<RegistrationForm> ageValidator() {
-        return RegistrationForm.validator(x -> x.age, new NotBlankValidator());
+        return RegistrationForm.validator(form -> form.age, new NotBlankValidator());
     }
 
     private Validator<RegistrationForm> lastNameValidator() {
-        return RegistrationForm.validator(x -> x.lastName, new NotBlankValidator());
+        return RegistrationForm.validator(form -> form.lastName, new NotBlankValidator());
     }
 
     private Validator<RegistrationForm> firstNameValidator() {
-        return RegistrationForm.validator(x -> x.firstName, new NotBlankValidator());
+        return RegistrationForm.validator(form -> form.firstName, new NotBlankValidator());
     }
 }
