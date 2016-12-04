4ea7cf00c70966b06ee5981d9a2bfe8fbf80ca2f
Liam Williams <liam.williams@zoho.com>
2016-12-04 15:20:51 +0000

    Fix build

--- src/test/java/com/github/javaparser/ast/visitor/DumpVisitor.java
+++ src/test/java/com/github/javaparser/ast/visitor/DumpVisitor.java
@@ -1,24 +1,20 @@
 /*
- * Copyright (C) 2007-2010 Júlio Vilmar Gesser.
- * Copyright (C) 2011, 2013-2016 The JavaParser Team.
+ * Copyright 2016 Liam Williams <liam.williams@zoho.com>.
  *
- * This file is part of JavaParser.
+ * This file is part of business-flows.
  *
- * JavaParser can be used either under the terms of
- * a) the GNU Lesser General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- * b) the terms of the Apache License
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * You should have received a copy of both licenses in LICENCE.LGPL and
- * LICENCE.APACHE. Please refer to those files for details.
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
- * JavaParser is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package com.github.javaparser.ast.visitor;
 
 import static com.github.javaparser.PositionUtils.sortByBeginPosition;
@@ -1667,7 +1663,7 @@ public class DumpVisitor implements VoidVisitor<Object> {
         sortByBeginPosition(everything);
         int positionOfTheChild = -1;
         for (int i=0;i<everything.size();i++){
-            if (everything.get(i)==node) positionOfTheChild=i;
+            if (everything.get(i).equals(node)) positionOfTheChild=i;
         }
         if (positionOfTheChild==-1) throw new RuntimeException("My index not found!!! "+node);
         int positionOfPreviousChild = -1;
